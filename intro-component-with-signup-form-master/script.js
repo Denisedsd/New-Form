baa1278443e7f7e529e01de6daca075d8078e72e
Denise Dias <111537830+Denisedsd@users.noreply.github.com>
2022-09-30 23:46:51 -0300

    "Adeed JS Function input."

---
+++ intro-component-with-signup-form-master/script.js
@@ -0,0 +1,14 @@
+function cadastro() {
+
+var nome = document.getElementById("firstname").value;
+var sobrenome = document.getElementById("lastname").value;
+var email = document.getElementById("email").value;
+var senha = document.getElementById("password").value;
+
+
+alert ("Bem vindo(a) "+ nome + " " + sobrenome);
+
+}
+
+
+
